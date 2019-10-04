e5ccd865cfae8f9236d965267c40aeda03dccf3e
Yuki Kikuchi <bclswl0827@foxmail.com>
2019-10-04 17:39:38 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Clear previous configuration
 cat /dev/null > /etc/v2ray/config.json
 # V2Ray new configuration
 echo "$CONFIG_JSON" > /etc/v2ray/config.json
+# Run V2Ray
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
